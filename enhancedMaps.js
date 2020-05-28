abc06e0518c45dd44eb08372395cc76fb5c1605b
Xavier <x.deneux@gmail.com>
2020-05-28 22:16:16 +0200

    Make new version to work this new strava route builder

--- enhancedMaps.js
+++ enhancedMaps.js
@@ -1,194 +1,265 @@
-var routeBuilderOpts = jQuery('#view-options li.map-style div.switches');
-if (routeBuilderOpts.length) {
-	var $ = jQuery;
+// var routeBuilderExist = $('#ftue-map-settings');
+console.log('enhancedMaps', document.arrive)
 
-	class EnhancedMaps {
-		allMaps = [
-			{
-				id: "geoportail",
-				name: "IGN Satellite",
-				tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
-			},
-			{
-				id: "ign-classic",
-				name: "IGN Classique",
-				tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}"
-			},
-			{
-				id: "ign",
-				name: "IGN",
-				tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
-			},
-			{
-				id: "igntopo",
-				name: "IGN Topo",
-				tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=CADASTRALPARCELS.PARCELLAIRE_EXPRESS&style=PCI%20vecteur&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix={z}&TileCol={x}&TileRow={y}"
-			},
-			{
-				id: "openstreetmap",
-				name: "OpenStreetMap",
-				tileUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			},
-			{
-				id: "opencyclemap",
-				name: "OpenCycleMap",
-				tileUrl: "https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png"
-			},
-			{
-				id: "transport",
-				name: "Transport",
-				tileUrl: "https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png"
-			},
-			{
-				id: "outdoors",
-				name: "Outdoors",
-				tileUrl: "https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+class EnhancedMaps {
+	allMaps = [
+		{
+			id: "geoportail",
+			name: "IGN Satellite",
+			tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=ORTHOIMAGERY.ORTHOPHOTOS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
+		},
+		{
+			id: "ign-classic",
+			name: "IGN Classique",
+			tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}"
+		},
+		{
+			id: "ign",
+			name: "IGN",
+			tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}",
+		},
+		{
+			id: "igntopo",
+			name: "IGN Topo",
+			tileUrl: "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/wmts?layer=CADASTRALPARCELS.PARCELLAIRE_EXPRESS&style=PCI%20vecteur&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&TileMatrix={z}&TileCol={x}&TileRow={y}"
+		},
+		{
+			id: "openstreetmap",
+			name: "OpenStreetMap",
+			tileUrl: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+		},
+		{
+			id: "opencyclemap",
+			name: "OpenCycleMap",
+			tileUrl: "https://a.tile.thunderforest.com/cycle/{z}/{x}/{y}.png"
+		},
+		{
+			id: "transport",
+			name: "Transport",
+			tileUrl: "https://a.tile.thunderforest.com/transport/{z}/{x}/{y}.png"
+		},
+		{
+			id: "outdoors",
+			name: "Outdoors",
+			tileUrl: "https://a.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+		}
+	];
+
+	reactInstance;
+	mapInstance;
+	opacity = +localStorage.getItem('opacity');
+	currentStravaFirstMap = localStorage.getItem('currentStravaFirstMap');
+	currentStravaSecondMap = localStorage.getItem('currentStravaSecondMap');
+	originalLayers = [];
+
+	findReact(dom, traverseUp = 0) {
+		const key = Object.keys(dom).find(key=>key.startsWith("__reactInternalInstance$"));
+		const domFiber = dom[key];
+
+		if (domFiber == null) return null;
+
+		// react 16+
+		const GetCompFiber = fiber=>{
+			let parentFiber = fiber.return;
+			while (typeof parentFiber.type == "string") {
+				parentFiber = parentFiber.return;
 			}
-		];
-
-		allTileLayers = {};
-		maps = [];
-
-		opacity = +localStorage.getItem('opacity') || 50;
-		currentStravaFirstMap = localStorage.getItem('currentStravaFirstMap');
-		currentStravaSecondMap = localStorage.getItem('currentStravaSecondMap');
-
-		constructor(){
-			this.allMaps.forEach((map)=>{
-				this.allTileLayers[map.id] =  L.tileLayer(map.tileUrl, {attribution: ""});
-			});
-
-			this.checkLocalStorageValues();
+			return parentFiber;
+		};
+		let compFiber = GetCompFiber(domFiber);
+		for (let i = 0; i < traverseUp; i++) {
+			compFiber = GetCompFiber(compFiber);
 		}
 
-		checkLocalStorageValues(){
-			if(!this.currentStravaFirstMap || this.currentStravaFirstMap == 'undefined' || !this.allTileLayers[this.currentStravaFirstMap]){
-				this.setFirstMap(this.allMaps[0].id);
-			}
+		return compFiber;
+	}
 
-			if(!this.currentStravaSecondMap || this.currentStravaSecondMap == 'undefined' || !this.allTileLayers[this.currentStravaSecondMap]){
-				this.setSecondMap(this.allMaps[1].id);
-			}
 
-			if(!this.opacity || this.opacity < 0 || this.opacity > 100){
-				this.setOpacity(50);
-			}
+	sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
+	async wait(what) {
+		for (let tries = 0, delay = 100; tries < 60; ++tries, delay = Math.min(delay * 2, 1000)) {
+			const got = what();
+			if (got)
+				return got;
+
+			await this.sleep(delay);
 		}
 
-		getLayer(mapId){
-			let mapConfig = this.allMaps.filter((map)=>{
-				return map.id === mapId;
-			})[0];
+		throw new Error(`timeout ${what}`);
+	}
 
-			return L.tileLayer(mapConfig.tileUrl, {attribution: ""});
-		}
 
-		setMap(firstMap){
-			this.mainMap = firstMap;
-			this.addLayers(firstMap);
-			this.maps[0] = firstMap.instance;
+	sourceFromLeaflet(l) {
+		const s = {
+			type: "raster",
+			tiles: [l.tileUrl],
+			minzoom: l.opts && l.opts.minZoom ? l.opts.minZoom : 0,
+			maxzoom: l.opts && l.opts.maxNativeZoom ? l.opts.maxNativeZoom : l.opts && l.opts.maxZoom ? l.opts.maxZoom : 18,
+			tileSize: 256,
+			attribution: '',
+		};
+		return s;
+	}
 
-			this.createSelects();
-			this.onChangeMap();
-			this.updateDisplayOpacity();
-		}
+	layerFromLeaflet(id, firstOrSecondMap) {
+		const l = this.allMaps.find((map)=>map.id==id)
+		const before = this.mapInstance.getLayer("global-heatmap") ? "global-heatmap" : "z-index-1";
+		const map = this.mapInstance;
 
-		changeOpacity(e){
-			this.setOpacity(e.target.value);
-			this.updateDisplayOpacity();
-		}
+		if (!map.getSource(id))
+			map.addSource(id, this.sourceFromLeaflet(l));
+		if (map.getLayer(firstOrSecondMap))
+			map.removeLayer(firstOrSecondMap);
+		map.addLayer({id: firstOrSecondMap, type: "raster", source: id}, before);
+	}
 
-		setOpacity(opacity){
-			this.opacity = opacity;
-			localStorage.setItem('opacity', opacity );
-		}
 
-		updateDisplayOpacity(){
-			$('#opacityValue').html(this.opacity);
-			$('#map-canvas .leaflet-layer').css('opacity', 1);
-			$('#map-canvas .leaflet-layer:last-child()').css('opacity', this.opacity/100);
-		}
+	resetLayers(){
+		this.mapInstance.getStyle().layers.filter(l => l.source == "composite").map(l => l.id).forEach(l => this.mapInstance.removeLayer(l));
+	}
 
-		createSelects(){
-			let selectFirstMap = $('<select name="firstMap" id="firstMapSelect"></select>');
-			selectFirstMap.on('change', this.onChangeMap.bind(this));
-			let selectSecondMap = $('<select name="secondMap" id="secondMapSelect"><option value=""></option></select>');
-			selectSecondMap.on('change', this.onChangeMap.bind(this));
+	setFirstMap(id){
+		if (!id && !this.allMaps.find((map)=>map.id==id))
+			return;
+		localStorage.currentStravaFirstMap = id;
+		this.resetLayers();
+		this.layerFromLeaflet(id, "primary");
+	}
 
-			this.allMaps.forEach(l => {
-				let optionFirstSelect = $('<option value="'+l.id+'">'+l.name+'</option>');
-				let optionSecondSelect = $('<option value="'+l.id+'">'+l.name+'</option>');
-				if(l.id === this.currentStravaFirstMap){
-					optionFirstSelect.attr('selected', 'selected');
+	setSecondMap(id){
+		console.log('id', id)
+		if (!id && !this.allMaps.find((map)=>map.id==id))
+			return;
+		localStorage.currentStravaSecondMap = id;
+		this.resetLayers();
+		this.layerFromLeaflet(id, "secondary");
+		this.onOpacityChange();
+	}
+
+
+	onChangeMap(){
+		setTimeout(() => {
+			if($('#firstMapSelect').val()){
+				this.setFirstMap($('#firstMapSelect').val());
+
+				if($('#secondMapSelect').val()){
+					$('#opacity').show();
+					this.setSecondMap($('#secondMapSelect').val());
+				}else{
+					$('#opacity').hide();
 				}
-				if(l.id === this.currentStravaSecondMap){
-					optionSecondSelect.attr('selected', 'selected');
-				}
+			}else{
+				$('#secondMapSelect').val('');
+				$('#opacity').hide();
+			}
+		});
+	}
 
-				selectFirstMap.append(optionFirstSelect);
-				selectSecondMap.append(optionSecondSelect);
-			});
-			$('.map-style .label').remove();
-			$('.map-style .switches')
-				.html('')
-				.append('<div class="label">Carte premier plan</div>')
-				.append(selectFirstMap)
+	onOpacityChange(){
+		let opacityInput = $('#opacity input').val();
+		let opacity = opacityInput ? parseFloat(opacityInput) : null;
 
-			let opacityInput = $('<input type="range" value="'+this.opacity+'" max="100" min="0" step="1" class="slider">');
-			opacityInput.on('change', {event}, this.changeOpacity.bind(this));
-			$('#view-options .map-style')
-				.append('<div class="label">Carte second plan</div>')
-				.append(selectSecondMap)
-				.append($('<span id="opacity"><span class="label">Opacité (<span id="opacityValue">'+this.opacity+'</span>%)</span><div id="opacitySlider">')
-				.append(opacityInput)
-				.append('</div></span>'));
-		}
-
-		setFirstMap(map){
-			localStorage.setItem('currentStravaFirstMap', map);
-			this.currentStravaFirstMap = map;
-		}
-
-		setSecondMap(map){
-			localStorage.setItem('currentStravaSecondMap', map);
-			this.currentStravaSecondMap = map;
-		}
-
-		onChangeMap(){
-			this.setFirstMap($('#firstMapSelect').val());
-			let secondMapVal = $('#secondMapSelect').val();
-
-			this.mainMap.setLayer(this.currentStravaFirstMap);
-
-			if(secondMapVal){
-				this.maps[0].addLayer(
-					this.getLayer(secondMapVal)
+		if($('#secondMapSelect').val()){
+			if(typeof(opacity)==='number'){
+				this.opacity = opacity/100;
+				localStorage.setItem('opacity', opacity );
+				this.mapInstance.setPaintProperty(
+					'secondary',
+					'raster-opacity',
+					this.opacity
 				);
-			}
-
-			this.updateDisplayOpacity();
-		}
-
-		addLayers(map) {
-			map.layers.runbikehike = map.createLayer("run-bike-hike");
-
-			for(var id in this.allTileLayers){
-				map.layers[id] = this.allTileLayers[id];
+				$('#opacityValue').html(opacity);
+				$('#opacity input').val(opacity);
 			}
 		}
 	}
 
-	var enhancedMap = new EnhancedMaps();
+	getFirstMap(){
+		return localStorage.currentStravaFirstMap || '';
+	}
 
-	Strava.Routes.MapViewOptionsView.prototype.setMapStyle = function(t){
- 		var map = this.map;
+	getSecondMap(){
+		return localStorage.currentStravaSecondMap || '';
+	}
+
+	drawUI(){
+		let node = $('<div class="Sidebar--section--r_mne" id="strava-enhanced-maps"><h4 class="text-callout text-demi mt-0 mb-0">Strava Enhanced Maps</h4><div id="mapSelects"></div></div>');
+		let selectFirstMap = $('<select name="firstMap" id="firstMapSelect"><option value=""></option></select>');
+		selectFirstMap.on('change', this.onChangeMap.bind(this));
+		let selectSecondMap = $('<select name="secondMap" id="secondMapSelect"><option value=""></option></select>');
+		selectSecondMap.on('change', this.onChangeMap.bind(this));
+
+		$('#main>div:nth-child(2)').prepend(node)
+		$('#mapSelects').append('<div><span>1<sup>er</sup> plan</span></div>');
+		$('#mapSelects div:first').append(selectFirstMap);
+		$('#mapSelects').append('<div><span>2<sup>nd</sup> plan</span></div>');
+		$('#mapSelects div:nth-child(2)').append(selectSecondMap);
+
+		let opacityInput = $('<input type="range" max="100" value="'+this.opacity+'" min="0" step="1" class="slider">');
+		opacityInput.on('change', {event}, this.onOpacityChange.bind(this));
+
+		$('#strava-enhanced-maps').append(
+			$('<div id="opacity"><span class="label">Opacité (<span id="opacityValue"></span>%)</span><div id="opacitySlider"></div></div>')
+		);
+
+		$('#opacitySlider').append(opacityInput);
+		if(!this.currentStravaSecondMap){
+			$('#opacity').hide();
+		}
+
+		this.allMaps.forEach(l => {
+			let optionFirstSelect = $('<option value="'+l.id+'">'+l.name+'</option>');
+			let optionSecondSelect = $('<option value="'+l.id+'">'+l.name+'</option>');
+
+			if(l.id === this.currentStravaFirstMap){
+				optionFirstSelect.attr('selected', 'selected');
+			}
+			if(l.id === this.currentStravaSecondMap){
+				optionSecondSelect.attr('selected', 'selected');
+			}
+
+			$('#firstMapSelect').append(optionFirstSelect);
+			$('#secondMapSelect').append(optionSecondSelect);
+		});
+
+		// let button = $('<button type="button">Test</button>');
+		// button.on('click', this.test.bind(this));
+		// $('#main>div:nth-child(2)').append(button)
+	}
+
+	checkLocalStorageValues(){
+		if(!this.currentStravaFirstMap || this.currentStravaFirstMap == 'undefined'){
+			this.currentStravaFirstMap = this.allMaps[2].id;
+		}
+
+		if(!this.opacity || this.opacity < 0 || this.opacity > 100){
+			this.opacity = 50;
+		}
+	}
+
+	async init(){
+		this.reactInstance = this.findReact(document.getElementsByClassName('mapboxgl-map')[0]);
+		this.checkLocalStorageValues();
+
+		await this.wait(() => {
+			this.reactInstance.return.memoizedProps.mapboxRef((m) => (this.mapInstance = m, m));
+				return this.mapInstance;
+			}
+		);
+
+		this.originalLayers = this.mapInstance.getStyle().layers;
+		this.drawUI();
 
 		setTimeout(()=>{
-			enhancedMap.setMap(this.map);
-		},500)
-
-		return map.setLayer(t);
-	};
-
-	routeBuilderOpts.children()[1].click();
+			this.onChangeMap();
+		},1000)
+	}
 }
+
+document.arrive(".mapboxgl-map", {onceOnly: false, existing: true}, function () {
+	var enhancedMap = new EnhancedMaps();
+	enhancedMap.init();
+});
--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 (function(){
-	if(document.getElementsByClassName('leaflet-container').length){
-		var s = document.createElement("script");
-		s.src = chrome.runtime.getURL('enhancedMaps.js');
-		s.type = 'text/javascript';
-		document.body.appendChild(s);
+		let scriptsToLoad = 2;
+		let scriptLoaded = 0;
 
-		var styles = document.createElement('link');
-		styles.rel = 'stylesheet';
-		styles.type = 'text/css';
-		styles.media = 'screen';
-		styles.href = chrome.runtime.getURL("style.css");
-		document.body.appendChild(styles);
-	}
+		scriptIsLoaded = () => {
+			scriptLoaded++;
+			if(scriptLoaded === scriptsToLoad){
+				var s = document.createElement("script");
+				s.src = chrome.runtime.getURL('enhancedMaps.js');
+				s.type = 'text/javascript';
+				document.body.appendChild(s);
+			}
+		}
+
+		if(document.location.href === "https://www.strava.com/routes/new?v2=true"){
+			let jquery = document.createElement("script");
+			jquery.src = chrome.runtime.getURL("lib/jquery-3.5.1.min.js");
+			jquery.type = "text/javascript";
+			jquery.onload = () => scriptIsLoaded();
+			document.body.appendChild(jquery);
+
+			let arrive = document.createElement('script');
+			arrive.src = chrome.runtime.getURL('lib/arrive.min.js');
+			arrive.type = 'text/javascript';
+			arrive.onload = () => scriptIsLoaded();
+			document.body.appendChild(arrive);
+
+
+			var styles = document.createElement('link');
+			styles.rel = 'stylesheet';
+			styles.type = 'text/css';
+			styles.media = 'screen';
+			styles.href = chrome.runtime.getURL("style.css");
+			document.body.appendChild(styles);
+		}
+
 })();
